f8ba087fa6e84408e5517b66aaf49e7c585e700e
Hassan-Ilyas-Virk <hassanilyas.virk@gmail.com>
2025-08-04 19:48:42 +0500

    Fix build error: update Scene import path and browserslist database

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import About from '@/components/About/About'
 import Projects from '../components/Projects/Projects.js'
 import Contact from '../components/Contact/contact.js'
 import "../components/Contact/contact.css";
-const Scene = dynamic(() => import('@/components/Scene'), {
+const Scene = dynamic(() => import('@/components/Scene/Index'), {
     ssr: false,
 })
 
